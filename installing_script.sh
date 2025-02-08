a3f8630342c9ec6f2af5938c251b97f9f841b0a4
Harshalkachewar <111994225+Harshalkachewar@users.noreply.github.com>
2025-02-08 10:21:41 +0530

    Update installing_script.sh

--- installing_script.sh
+++ installing_script.sh
@@ -1,61 +1,62 @@
 #!/bin/bash
 
-#docker
+
+sudo apt update -y
+#sudo apt upgrade -y
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update -y
+sudo apt install temurin-17-jdk -y
+/usr/bin/java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+                  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+                              /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+# Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install docker.io -y
-sudo usermod -aG docker $USER  # Replace with your system's username, e.g., 'ubuntu'
+sudo usermod -aG docker $USER   #my case is ubuntu
+newgrp docker
 sudo chmod 777 /var/run/docker.sock
+systemctl restart docker
+systemctl enable docker
 
-#sonarqube
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
-#trivy
-sudo apt-get install wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
-sudo apt-get install trivy
+sudo apt-get install trivy -y
 
-#java
-sudo apt update
-sudo apt install fontconfig openjdk-17-jre
-java -version
-openjdk version "17.0.8" 2023-07-18
-OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
-
-#jenkins
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-
-#monitor-prom
-
-sudo useradd --system --no-create-home --shell /bin/false prometheus
+sudo useradd \
+    --system \
+    --no-create-home \
+    --shell /bin/false prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.47.1/prometheus-2.47.1.linux-amd64.tar.gz
 tar -xvf prometheus-2.47.1.linux-amd64.tar.gz
-cd prometheus-2.47.1.linux-amd64/
 sudo mkdir -p /data /etc/prometheus
+cd prometheus-2.47.1.linux-amd64/
 sudo mv prometheus promtool /usr/local/bin/
 sudo mv consoles/ console_libraries/ /etc/prometheus/
 sudo mv prometheus.yml /etc/prometheus/prometheus.yml
 sudo chown -R prometheus:prometheus /etc/prometheus/ /data/
-
+cd
+rm -rf prometheus-2.47.1.linux-amd64.tar.gz
+prometheus --version
 
 echo "[Unit]
 Description=Prometheus
 Wants=network-online.target
 After=network-online.target
-
 StartLimitIntervalSec=500
 StartLimitBurst=5
-
 [Service]
 User=prometheus
 Group=prometheus
@@ -69,46 +70,46 @@ ExecStart=/usr/local/bin/prometheus \
   --web.console.libraries=/etc/prometheus/console_libraries \
   --web.listen-address=0.0.0.0:9090 \
   --web.enable-lifecycle
-
 [Install]
-WantedBy=multi-user.target" >>  /etc/systemd/system/prometheus.service
+WantedBy=multi-user.target" > /etc/systemd/system/prometheus.service
+
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl status prometheus
 
-#monitor-nodeexporter
-sudo useradd --system --no-create-home --shell /bin/false node_exporter
+sudo useradd \
+    --system \
+    --no-create-home \
+    --shell /bin/false node_exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz
 tar -xvf node_exporter-1.6.1.linux-amd64.tar.gz
-sudo mv node_exporter-1.6.1.linux-amd64/node_exporter /usr/local/bin/
+sudo mv \
+  node_exporter-1.6.1.linux-amd64/node_exporter \
+  /usr/local/bin/
 rm -rf node_exporter*
+node_exporter --version
 
 echo "[Unit]
 Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
-
 StartLimitIntervalSec=500
 StartLimitBurst=5
-
 [Service]
 User=node_exporter
 Group=node_exporter
 Type=simple
 Restart=on-failure
 RestartSec=5s
-ExecStart=/usr/local/bin/node_exporter --collector.logind
-
+ExecStart=/usr/local/bin/node_exporter \
+    --collector.logind
 [Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/node_exporter.service
+WantedBy=multi-user.target" > /etc/systemd/system/node_exporter.service
 
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 sudo systemctl status node_exporter
 
-#monitor-grafana
-
-sudo apt-get update
 sudo apt-get install -y apt-transport-https software-properties-common
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
@@ -117,5 +118,4 @@ sudo apt-get -y install grafana
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 sudo systemctl status grafana-server
-
 
